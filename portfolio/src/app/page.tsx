b039450b2ffee4c7bc61512c209b69380e88df07
Ryan <ryan.hercule@epitech.eu>
2025-10-25 16:17:03 +0700

    :lipstick:(home page): finish primary style

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,27 +1,25 @@
 import ProfilePic from "@/components/Home/ProfilePic";
 import ReachMeSection from "@/components/Home/ReachMeSection";
+import Role from "@/components/Home/Role";
 
 function Home() {
   return (
-    <div className="flex justify-start space-x-36 items-center"> 
-      <div className="space-y-4">
-        <h1
-          className={
-            "text-[2.5rem] font-extrabold leading-tight text-slate-700 md:text-7xl md:leading-snug dark:text-slate-300"
-          }
-        >
-          {"Hi, I'm Ryan HERCULE."}
-        </h1>
-        <p
-          className={
-            "mt-4 text-lg text-slate-600 md:mt-6 md:text-2xl lg:max-w-[500px] xl:max-w-[700px] dark:text-slate-400"
-          }
-        >
-          {"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in 1500."}
-        </p>
+    <div className="flex flex-wrap justify-between items-center sm:p-16 p-4 sm:mb-26 sm:mx-12"> 
+      <div className="flex flex-col space-y-12">
+        <div className="mx-auto max-w-2xl text-left text-4xl font-medium leading-tight tracking-tighter text-text-primary md:text-6xl">
+          <h1>
+            Hi! 👋🏻<br />
+            <span className="text-dark-blue">Ryan HERCULE</span>
+          </h1>
+        </div>
+        <div className="text-dark-blue text-2xl">
+          <Role />
+        </div>
         <ReachMeSection />
       </div>
-      <ProfilePic />
+      <div className="pr-24">
+        <ProfilePic />
+      </div>
     </div>
   );
 }
--- portfolio/src/components/Navigation/NavBar.tsx
+++ portfolio/src/components/Navigation/NavBar.tsx
@@ -17,14 +17,14 @@ function Navbar() {
    const path = usePathname();
 
   const getSelectedPageClass = (link: string): string => {
-   const style = "font-medium hover:text-blue-600 flex h-8 w-full items-center justify-center p-4";
-    return path === link ? `${style} text-blue-600 bg-blue-100 rounded-full` : `${style} text-black-500`;
+   const style = "font-medium hover:text-blue-primary flex h-8 w-full items-center justify-center p-4";
+    return path === link ? `${style} text-blue-primary bg-blue-secondary rounded-full` : `${style} text-text-primary`;
   };
 
   return (
    <div className="flex space-x-4">
       <Logo />
-      <ul className="flex items-center space-x-2 text-sm">
+      <ul className="flex items-center space-x-2 text-sm text-text-primary">
       {navLinks.map((link) => (
          <li key={link.name}>
             <Link

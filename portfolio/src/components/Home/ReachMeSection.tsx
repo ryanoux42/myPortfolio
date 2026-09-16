237469459e6a51312c44fa30d5479e5cc460a5a3
Ryan <ryan.hercule@epitech.eu>
2026-09-16 21:24:05 +0200

    :bug:(home page): update CV download link

--- portfolio/src/components/Home/ReachMeSection.tsx
+++ portfolio/src/components/Home/ReachMeSection.tsx
@@ -3,7 +3,7 @@ import ButtonWithLink from "@/components/Button/ButtonWithLink";
 import { FiDownload } from "react-icons/fi";
 import { HiOutlineMail } from 'react-icons/hi';
 
-const CV_LINK = "https://drive.google.com/file/d/1u8qMDBxg1xEP9BF_Lso6u4peXGKyxHpb/view?usp=sharing";
+const CV_LINK = "https://drive.google.com/file/d/1Vw2msGl9PAZh6fCgtKINRb5uTle7OgYA/view?usp=sharing";
 
 function ReachMeSection() {
   return (
